5d6145dff3dbca04ac4e3087e6e22f45eca8d61f
Alvin Cheng <eventide1029@gmail.com>
2026-06-25 18:26:11 +1000

    Initial Commit

---
+++ Esplanade/EsplanadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsplanadeApp.swift
+//  Esplanade
+//
+//  Created by Alvin on 25/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct EsplanadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
